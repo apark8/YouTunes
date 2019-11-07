ce98fb2ae5b4cd79022ca39cdebac7ba505e8879
Austin Park <apark8@vols.utk.edu>
2019-11-07 00:59:10 -0500

    added animations for playing song

--- playlist.js
+++ playlist.js
@@ -1,7 +1,25 @@
+const anime = parent.require('animejs');
 const fs = parent.require('fs');
+
+function movePlayButton() {
+    const SPEED = 300;
+    playButtons = document.getElementsByClassName('playbtn');
+    var animation = anime.timeline({
+        easing: 'easeOutExpo',
+        duration: playButtons.length * SPEED
+    })
+    for (var i = 0; i < playButtons.length; i++) {
+        animation.add({
+            targets: playButtons[i],
+            scale: 0,
+            duration: SPEED
+        }, i * SPEED);
+    }
+}
 
 function play(path) {
     var audio = new Audio(path);
+    movePlayButton();
     audio.play();
 }
 
@@ -13,6 +31,8 @@ var files = fs.readdirSync(PATH);
 var table = document.getElementById('playlist');
 for (var i = 0; i < files.length; i++) {
     var row = table.insertRow(i);
-    var cell = row.insertCell(0);
-    cell.innerHTML = '<button onclick="play(\'' + PATH + files[i] + '\')">' + files[i] + '</button>';
+    var playbtn = row.insertCell(0);
+    var song = row.insertCell(1);
+    playbtn.innerHTML = '<button class="playbtn" onclick="play(\'' + PATH + files[i] + '\')"></button>';
+    song.innerHTML = files[i];
 }
